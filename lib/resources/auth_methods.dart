6cf9844b397957e0a14526c686e0d4e97c941219
Ismagod27 <20200098@miucateci.edu.do>
2024-12-04 22:58:47 -0400

    feat: update

--- lib/resources/auth_methods.dart
+++ lib/resources/auth_methods.dart
@@ -9,6 +9,7 @@ class AuthMethods {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
   Stream<User?> get authChanges => _auth.authStateChanges();
+  User get user => _auth.currentUser!;
 
   Future<bool> signInWithGoogle(BuildContext context) async {
     bool res = false;
@@ -43,5 +44,13 @@ class AuthMethods {
       res = false;
     }
     return res;
+  }
+
+  void signOut() async {
+    try {
+      _auth.signOut();
+    } catch (e) {
+      print(e);
+    }
   }
 }
